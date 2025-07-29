277b1375dd258ba12b536359574f2ef86715514d
Rakesh Vamsi Sagar Petla <rakeshsagar66@gmail.com>
2025-07-29 11:11:06 +0530

    Update CorsConfiguration.java

--- src/main/java/com/assignment/project/configuration/CorsConfiguration.java
+++ src/main/java/com/assignment/project/configuration/CorsConfiguration.java
@@ -9,7 +9,10 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                    "http://localhost:3000", 
+                    "https://rakeshsagar89.github.io"
+                )
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Location")
